ed01ce3d11253efd33ad284aaccc72b4c74ffd8c
hugodecasta <hugodecasta@orange.fr>
2019-12-20 20:46:23 +0100

    new version ...

--- lister.js
+++ lister.js
@@ -1,234 +1,201 @@
-// ECMA 5 compatible
+'use strict'
 
-// ------------------------------------- VARIABLES
+// ------------------------------------- GLOBAL DATA
 
-var update_time_ms = 1000;
+var bm = new BoolMaster('BoolMaster/api.php')
 
-// ------------------------------------- USES
+// ------------------------------------- LISTE NAME
 
-var liste = []
+async function get_current_liste_name() {
+    let name = localStorage.getItem('lister_current_list_name')
 
-var storage_name = 'lister_liste_name'
-
-function liste_name_is_null(name) {
-    return (name == null || name == 'null')
-}
-
-function liste_name() {
-    var name = localStorage.getItem(storage_name)
-    while(liste_name_is_null(name)) {
-        name = prompt("Nom de la liste", "");
-        set_liste_name(name)
+    if(name == null) {
+        let prompted_name = await prompt_list_name()
+        if(prompted_name == '') {
+            prompted_name = null
+        }
+        await set_current_list_name(prompted_name)
+        return await get_current_liste_name()
     }
+
+    if(! await bm.key_exists(name)) {
+        let create = confirm('la liste "'+name+'" n\'existe pas, souhaitez vous la créer ?')
+        if(create) {
+            await set_liste({}, name)
+        } else {
+            await set_current_list_name(null)
+        }
+        return await get_current_liste_name()
+    }
+
     return name
 }
 
-function prompt_rename_list() {
-    var current_name = liste_name()
-    var new_name = prompt("Nouveau nom de liste",liste_name())
-    if(liste_name_is_null(new_name) || new_name == current_name)
+async function set_current_list_name(liste_name) {
+    if(liste_name == null) {
+        localStorage.removeItem('lister_current_list_name')
         return
-    rename_liste(new_name,function(done_name){
-        if(done_name != new_name) {
-            alert('Le nom "'+new_name+'" est déjà existant')
-        } else {
-            update_liste()
-        }
-    })
-}
-
-function set_liste_name(name) {
-    localStorage.setItem(storage_name, name)
-}
-
-function reset_liste_name() {
-    localStorage.setItem(storage_name, null)
-}
-
-// ------------------------------------- REQ
-
-function call(attrs,ret) {
-    $.get('api1.php?'+attrs)
-    .done(function(data){
-        ret(data)
-    })
-}
-
-// ------------------------------------- ACTIONS
-
-function remove(id) {
-    for(var i in liste) {
-        var liste_item = liste[i]
-        var item_id = liste_item.id
-        if(item_id == id) {
-            liste.splice(i,1)
-            save_liste()
-            break
-        }
     }
+    localStorage.setItem('lister_current_list_name', liste_name)
 }
 
-function add_item() {
-    var text = prompt("Nouvel élément", "");
-    if(text == null)
-        return
-    var liste_item = {text:text,id:Date.now()+Math.random()}
-    liste.push(liste_item)
-    save_liste(draw_list)
+// ------------------------------------- LISTE
+
+async function prompt_list_name(name='') {
+    let given_name = prompt('nom de la liste',name)
+    return given_name
 }
 
-// ------------------------------------- GX
+async function get_liste() {
+    let name = await get_current_liste_name()
+    let liste = await bm.read_key(name)
+    return liste
+}
 
-function draw_list() {
-    $('.renBtn').html(liste_name())
-    $('.content').html('')
-    var rev_list = liste.slice().reverse();
-    for(var key in rev_list) {
-        var data = rev_list[key]
-        $('.content').append(create_item_GX(data))
+async function set_liste(liste, given_name=null) {
+    let name = given_name==null?await get_current_liste_name():given_name
+    return await bm.write_key(name, liste)
+}
+
+async function remove_liste() {
+    let name = await get_current_liste_name()
+    return await bm.key_remove(name)
+}
+
+// ------------------------------------- ITEM
+
+async function create_item_obj(item_name) {
+    let id = Math.random()+''+Date.now()
+    let item_obj = {
+        id:id,
+        text:item_name
     }
+    return item_obj
 }
 
-function create_item_GX(liste_item) {
+async function set_item(item) {
+    let liste = await get_liste()
+    liste[item.id] = item
+    await set_liste(liste)
+}
 
-    var text = liste_item.text
-    var id = liste_item.id
+async function remove_item(item) {
+    let liste = await get_liste()
+    delete liste[item.id]
+    await set_liste(liste)
+}
 
-    var item = $('<div>').addClass('item')
-    var textJQ = $('<div>').addClass('text').html(text)
+// ------------------------------------- ITEM MANAGEMENT
+
+function init_item(name, item) {
+
+    var itemJQ = $('<div>').addClass('item')
+    var textJQ = $('<div>').addClass('text')
     var btnJQ = $('<div>').addClass('trash')
 
-    item.append(textJQ).append(btnJQ)
+    itemJQ.append(textJQ).append(btnJQ)
 
-    item.click(function(){
-        if(item.hasClass('edit'))
-            item.removeClass('edit')
+    itemJQ.click(function(){
+        if(itemJQ.hasClass('edit'))
+            itemJQ.removeClass('edit')
         else
-            item.addClass('edit')
+            itemJQ.addClass('edit')
     })
 
-    btnJQ.click(function(){
-        remove(id)
-        item.addClass('disappear')
+    btnJQ.click(async function(){
+        await remove_item(item)
+        bm.trigger_checker(name+':'+item.id+'!removed')
+    })
+
+    bm.register_checker(name+':'+item.id+':text!changed',function(prop, text) {
+        textJQ.html(text)
+        itemJQ.addClass('appear')
+        setTimeout(function() {
+            itemJQ.removeClass('appear')
+        },1000)
+    })
+
+    bm.register_checker(name+':'+item.id+'!removed',function() {
+        itemJQ.addClass('disappear')
         setTimeout(function(){
-            item.remove()
+            itemJQ.remove()
         },300)
     })
 
-    return item
+    $('.content').append(itemJQ)
+
 }
 
-// ------------------------------------- DATA PERENITY
+// ------------------------------------- LISTE INIT
 
-function save_liste(callback) {
-    boolMaster_write_key(liste_name(), liste, callback)
-}
+async function setup_liste() {
 
-function rename_liste(new_name, callback) {
-    load_liste(function(liste_data) {
-        boolMaster_key_exists(new_name,function(resp){
-            if(resp) {
-                callback(null)
-            } else {
-                remove_liste(function(){
-                    set_liste_name(new_name)
-                    save_liste(function(){
-                        callback(new_name)
-                    })
-                })
-            }
-        })
+    bm.reset_all_checker()
+
+    let name = await get_current_liste_name()
+    $('.renBtn').html(name)
+
+    $('.content').html('')
+
+    let add_id = bm.register_checker(name+'!added',function(id, item) {
+       init_item(name, item)
     })
+
+    let rem_id = bm.register_checker(name+'!removed',async function() {
+        console.log('removed !!')
+        await set_current_list_name(null)
+        await setup_liste()
+        bm.unregister_checker(add_id)
+        bm.unregister_checker(rem_id)
+     })
 }
 
-function load_liste(callback) {
+// ------------------------------------- GX ACTIONS
 
-    boolMaster_key_exists(liste_name(),function(resp){
-
-        if(!resp) {
-            var ok = confirm("La liste "+liste_name()+" n'existe pas, souhaitez vous la créer")
-            if(ok) {
-                save_liste(function(){
-                    callback([])
-                })
-            } else {
-                reset_liste_name()
-                load_liste(callback)
-            }
-        } else {
-            boolMaster_read_key(liste_name(),callback)
-        }
-    })
+async function gx_add_item() {
+    let item_name = prompt('nouvel élément')
+    let item_obj = await create_item_obj(item_name)
+    await set_item(item_obj)
 }
 
-function remove_liste(callback) {
-    var name = liste_name()
-    reset_liste_name()
-    boolMaster_key_remove(name, callback)
-}
-
-// ------------------------------------- UPDATERS
-
-var looping = false
-var loopingInterval = null
-
-function start_looper() {
-    looping = true
-    if(loopingInterval == null) {
-        loopingInterval = setInterval(function(){
-            if(!looping) {
-                clearInterval(loopingInterval)
-            }
-        },update_time_ms)
+async function gx_rename_liste() {
+    let current_name = await get_current_liste_name()
+    let new_name = await prompt_list_name(current_name)
+    if(new_name == current_name || new_name==null) {
+        return
     }
+    let liste = await get_liste()
+    await remove_liste()
+    await set_liste(liste,new_name)
+    await set_current_list_name(new_name)
+
+    await setup_liste()
 }
 
-function stop_looper() {
-    looping = false
+async function gx_new_liste() {
+    await set_current_list_name(null)
+    setup_liste()
 }
 
-function loop_updater(callback) {
-    load_liste(function(data_liste){
-        if(data_liste.length != liste.length) {
-            liste = data_liste
-            draw_list()
-        }
-        callback()
-    })
-}
-
-function update_liste() {
-    $('.content').html($('<div>').addClass('loading'))
-    load_liste(function(data_liste){
-        liste = data_liste
-        draw_list()
-    })
+async function gx_remove_liste() {
+    let name = await get_current_liste_name()
+    if(!confirm('souhaitez vous réellement supprimer la liste "'+name+'"')) {
+        return
+    }
+    await remove_liste()
 }
 
 // ------------------------------------- INIT
 
-function init() {
+async function main() {
+    
+    $('.addBtn').click(gx_add_item)
+    $('.renBtn').click(gx_rename_liste)
+    $('.newBtn').click(gx_new_liste)
+    $('.remBtn').click(gx_remove_liste)
 
-    init_boolMaster('hugocastaneda.fr/boolMaster2/api.php','https')
-    update_liste()
+    await setup_liste()
 
-    $('.addBtn').click(add_item)
-    $('.renBtn').click(prompt_rename_list)
-    $('.newBtn').click(function(){
-        reset_liste_name()
-        location.reload()
-    })
-    $('.remBtn').click(function(){
-        if(confirm('Souhaitez vous supprimer la liste "'+liste_name()+'"')) {
-            stop_looper()
-            remove_liste(function(){
-                update_liste()
-                start_looper()
-            })
-        }
-    })
-
-    start_looper()
 }
 
-setTimeout(init)
+main()
